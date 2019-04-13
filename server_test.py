7478d16f9c219ba1732516e543a6cb2a0ae99914
Vishal Patel <vishalpatel@Vishals-Air.fios-router.home>
2019-04-13 18:25:41 -0400

    cleanup

--- server_test.py
+++ server_test.py
@@ -7,137 +7,24 @@
 #	python simple_request.py
 
 # import the necessary packages
-from keras.applications import ResNet50
-from keras.preprocessing.image import img_to_array
-from keras.applications import imagenet_utils
-from keras.models import load_model
 from threading import Thread
 from PIL import Image
 import numpy as np
-import base64
 import flask
-import redis
-import uuid
-import time
-import json
 import sys
 import io
 
-# initialize constants used to control image spatial dimensions and
-# data type
-IMAGE_WIDTH = 224
-IMAGE_HEIGHT = 224
-IMAGE_CHANS = 3
-IMAGE_DTYPE = "float32"
+from process_image import *
+from classify import *
 
-# initialize constants used for server queuing
-IMAGE_QUEUE = "image_queue"
-BATCH_SIZE = 32
-SERVER_SLEEP = 0.25
-CLIENT_SLEEP = 0.25
+
 
 # initialize our Flask application, Redis server, and Keras model
 app = flask.Flask(__name__)
-db = redis.StrictRedis(host="localhost", port=6379, db=0)
-model = None
-
-def base64_encode_image(a):
-	# base64 encode the input NumPy array
-	return base64.b64encode(a).decode("utf-8")
-
-def base64_decode_image(a, dtype, shape):
-	# if this is Python 3, we need the extra step of encoding the
-	# serialized NumPy string as a byte object
-	if sys.version_info.major == 3:
-		a = bytes(a, encoding="utf-8")
-
-	# convert the string to a NumPy array using the supplied data
-	# type and target shape
-	a = np.frombuffer(base64.decodestring(a), dtype=dtype)
-	a = a.reshape(shape)
-
-	# return the decoded image
-	return a
-
-def prepare_image(image, target):
-	# if the image mode is not RGB, convert it
-	if image.mode != "RGB":
-		image = image.convert("RGB")
-
-	# resize the input image and preprocess it
-	image = image.resize(target)
-	image = img_to_array(image)
-	image = np.expand_dims(image, axis=0)
-	image = imagenet_utils.preprocess_input(image)
-
-	# return the processed image
-	return image
-
-def classify_process():
-    # load the pre-trained Keras model (here we are using a model
-    # pre-trained on ImageNet and provided by Keras, but you can
-    # substitute in your own networks just as easily)
-    print("* Loading model...")
-
-    model = load_model('inception_net_model')
-    print("* Model loaded")
-
-	# continually pool for new images to classify
-    while True:
-    	# attempt to grab a batch of images from the database, then
-    	# initialize the image IDs and batch of images themselves
-        queue = db.lrange(IMAGE_QUEUE, 0, BATCH_SIZE - 1)
-        imageIDs = []
-        batch = None
-
-        # loop over the queue
-        for q in queue:
-            # deserialize the object and obtain the input image
-            q = json.loads(q.decode("utf-8"))
-            image = base64_decode_image(q["image"], IMAGE_DTYPE,
-                (1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANS))
-
-        	# check to see if the batch list is None
-            if batch is None:
-                batch = image
-
-            # otherwise, stack the data
-            else:
-                batch = np.vstack([batch, image])
-
-            # update the list of image IDs
-            imageIDs.append(q["id"])
-
-        # check to see if we need to process the batch
-        if len(imageIDs) > 0:
-        	# classify the batch
-            print("* Batch size: {}".format(batch.shape))
-            preds = model.predict(batch)
-            # results = imagenet_utils.decode_predictions(preds)
-            results = preds
-        	# loop over the image IDs and their corresponding set of
-        	# results from our model
-            for (imageID, result) in zip(imageIDs, results):
-        		# # initialize the list of output predictions
-                label = 'hot dog' if result[0] > .5 else 'not_hot_dog'
-                output = []
-                r = {"label": label, "probability": float(result)}
-                output.append(r)
-
-                # store the output predictions in the database, using
-                # the image ID as the key so we can fetch the results
-                db.set(imageID, json.dumps(output))
-
-                # remove the set of images from our queue
-                db.ltrim(IMAGE_QUEUE, len(imageIDs), -1)
-
-                # sleep for a small amount
-                time.sleep(SERVER_SLEEP)
 
 @app.route("/predict", methods=["POST"])
 def predict():
-	# initialize the data dictionary that will be returned from the
-	# view
+	# initialize the data dictionary that will be returned from the view
 	data = {"success": False}
 
 	# ensure an image was properly uploaded to our endpoint
