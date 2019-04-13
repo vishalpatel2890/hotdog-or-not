e486dcb327cf32639b88df4633be95b2f68c6a95
Vishal Patel <vpatel2@NYC-US09851-MBP13.fios-router.home>
2019-04-13 13:11:20 -0400

    add server

--- server_test.py
+++ server_test.py
@@ -12,3 +12,16 @@ import time
 import json
 import sys
 import io
+
+# initialize constants used to control image spatial dimensions and
+# data type
+IMAGE_WIDTH = 224
+IMAGE_HEIGHT = 224
+IMAGE_CHANS = 3
+IMAGE_DTYPE = "float32"
+
+# initialize constants used for server queuing
+IMAGE_QUEUE = "image_queue"
+BATCH_SIZE = 32
+SERVER_SLEEP = 0.25
+CLIENT_SLEEP = 0.25
