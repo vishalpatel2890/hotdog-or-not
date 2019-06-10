afbdab82749075987b98d304ab131c41af845bea
Vishal Patel <vishal@happylifecoffee.com>
2019-06-10 18:51:33 -0400

    Update server_test.py

--- server_test.py
+++ server_test.py
@@ -82,6 +82,6 @@ if __name__ == "__main__":
 #to run on ec2
 #connect to ec2 instance
 #git clone repo to instance
-#pip install requirements.txt
+#pip install requirements.txt (pip3 install flask keras tensorflow numpy PIL redis
 #launch redis with redis-server (check with redis-cli ping)
-#launch server with sudo python server_test.py or nohup python3 server_test.py &
+#launch server with sudo python3 server_test.py or nohup python3 server_test.py &
